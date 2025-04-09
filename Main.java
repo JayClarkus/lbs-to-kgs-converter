d66a7b160440894f7517e9acfd16c8cfa8a45478
Jayden Clark <jaydenclark178@gmail.com>
2025-04-08 21:27:14 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,15 +4,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
     	
+    	boolean run = true;
     	Scanner scanner = new Scanner(System.in);
     	
-    	System.out.print("Enter the weight in lbs: ");
-    	int lbs = scanner.nextInt();
+    	while (run == true) {
+        	
+        	System.out.print("Enter the weight in lbs: ");
+        	
+        	if (scanner.hasNextDouble()) {
+        		double lbs = scanner.nextDouble();
+        		double kgs = lbs * 0.45359237;
+            	System.out.printf("%.2f lbs = %.2f kgs",lbs, kgs);
+            	
+            	scanner.close();
+            	run = false;
+        	}
+        	else {
+        		System.out.println("Invalid Input, try again...");
+        		System.out.println("------------------------------");
+        		scanner.nextLine();
+        	}
+    	}
     	
-    	double kgs = lbs * 0.45359237;
-    	System.out.printf("%d lbs = %.2f kgs",lbs, kgs);
-    	
-    	scanner.close();
     }
 
 }
